6b89a4e2592286b6b4defb3634332758eead1909
Quentin Gliech <quentingliech@gmail.com>
2017-12-21 10:23:08 +0100

    Show errors in examples

--- examples/stdin.rs
+++ examples/stdin.rs
@@ -6,7 +6,9 @@ use elayr::{xml_doc, Node};
 pub fn main() {
     let mut buffer = Vec::new();
     io::stdin().read_to_end(&mut buffer).unwrap();
-    let doc = xml_doc(buffer.as_slice());
-    let svg = Node::from_xml_doc(doc.unwrap().1);
-    println!("{}", svg.unwrap());
+    let doc = xml_doc(buffer.as_slice())
+        .to_full_result()
+        .expect("Failed to parse document");
+    let svg = Node::from_xml_doc(doc).expect("XML doc is not valid SVG");
+    println!("{}", svg);
 }
