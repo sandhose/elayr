db4c34572f4b8476080f5ef9873f99a30559f919
Quentin Gliech <quentingliech@gmail.com>
2017-12-22 01:42:25 +0100

    Add CommandType to command parsing in paths

--- src/path.rs
+++ src/path.rs
@@ -15,14 +15,15 @@ impl fmt::Display for Point {
 #[derive(Debug, PartialEq)]
 struct MoveTo {
     start: Point,
-    drawto_commands: Vec<DrawTo>,
+    cmd_type: CommandType,
+    commands: Vec<DrawTo>,
 }
 
 impl MoveTo {
     fn pretty_print(&self, f: &mut fmt::Formatter, depth: usize) -> fmt::Result {
         writeln!(f, "{0:1$}MoveTo {2}", "", depth * 2, self.start)?;
         let depth = depth + 1;
-        for ref command in &self.drawto_commands {
+        for ref command in &self.commands {
             writeln!(f, "{0:1$}{2:?}", "", depth * 2, command)?;
         }
         Ok(())
@@ -52,11 +53,23 @@ impl fmt::Display for Path {
     }
 }
 
+#[derive(Debug, PartialEq, Clone, Copy)]
+enum CommandType {
+    Relative,
+    Absolute
+}
+
+macro_rules! cmd (
+    ($i:expr, $abs:expr, $rel:expr) => (
+        alt!($i, value!(CommandType::Absolute, char!($abs)) | value!(CommandType::Relative, char!($rel)))
+    );
+);
+
 #[derive(Debug, PartialEq, Clone)]
 enum DrawTo {
     ClosePath,
-    LineTo(Point),
-    CurveTo(Point, Point, Point),
+    LineTo(CommandType, Point),
+    CurveTo(CommandType, Point, Point, Point),
 }
 
 named!(
@@ -90,33 +103,48 @@ named!(
 );
 
 named!(
-    moveto<Point>,
-    do_parse!(alt!(char!('M') | char!('m')) >> opt!(space) >> point: coordinate_pair >> (point))
+    moveto<(CommandType, Point)>,
+    do_parse!(cmd_type: cmd!('M', 'm') >> opt!(space) >> point: coordinate_pair >> ((cmd_type, point)))
 );
 
 named!(
     lineto<Vec<DrawTo>>,
     do_parse!(
-        alt!(char!('L') | char!('l')) >> opt!(space)
-            >> points:
-                map!(coordinate_list, |v| v.iter()
+        cmd_type: cmd!('L', 'l') >> 
+        opt!(space) >> 
+        points: map!(
+            coordinate_list, 
+            |v| v.iter()
                     .cloned()
-                    .map(DrawTo::LineTo)
-                    .collect()) >> (points)
+                    .map(|p| DrawTo::LineTo(cmd_type, p))
+                    .collect()
+        ) >> 
+        (points)
     )
 );
 named!(
     curveto<Vec<DrawTo>>,
     do_parse!(
-        alt!(char!('c') | char!('C')) >> commands: ws!(many1!(curveto_argument)) >> (commands)
+        cmd_type: cmd!('C', 'c') >> 
+        commands: ws!(many1!(curveto_argument)) >> 
+        (
+            commands.iter()
+                .cloned()
+                .map(|(p1, p2, p3)| DrawTo::CurveTo(cmd_type, p1, p2, p3))
+                .collect()
+        )
     )
 );
 
 named!(
-    curveto_argument<DrawTo>,
+    curveto_argument<(Point, Point, Point)>,
     do_parse!(
-        p1: coordinate_pair >> opt!(comma_wsp) >> p2: coordinate_pair >> opt!(comma_wsp)
-            >> p3: coordinate_pair >> (DrawTo::CurveTo(p1, p2, p3))
+        p1: coordinate_pair >>
+        opt!(comma_wsp) >>
+        p2: coordinate_pair >>
+        opt!(comma_wsp) >>
+        p3: coordinate_pair >>
+        ((p1, p2, p3))
     )
 );
 
@@ -128,18 +156,10 @@ named!(
 );
 
 named!(
-    drawto_commands<Vec<DrawTo>>,
-    do_parse!(
-        lines:
-            map!(coordinate_list, |v| v.iter()
-                .cloned()
-                .map(DrawTo::LineTo)
-                .collect::<Vec<_>>()) >> after: many0!(ws!(drawto_command))
-            >> (lines
-                .iter()
-                .chain(after.iter().flat_map(|c| c))
-                .cloned()
-                .collect())
+    commands<Vec<DrawTo>>,
+    map!(
+        many0!(ws!(drawto_command)),
+        |commands| commands.iter().flat_map(|c| c.clone()).collect()
     )
 );
 
@@ -151,9 +171,22 @@ named!(
 named!(
     moveto_drawto_command_group<MoveTo>,
     do_parse!(
-        start: moveto >> opt!(space) >> drawto_commands: drawto_commands >> (MoveTo {
-            start,
-            drawto_commands,
+        cmd: moveto >> 
+        opt!(space) >> 
+        implicite_linetos: coordinate_list >>
+        commands: commands >> 
+        ({
+            let (cmd_type, start) = cmd;
+            let commands = implicite_linetos.iter()
+                .map(|p| DrawTo::LineTo(cmd_type, p.clone()))
+                .chain(commands)
+                .collect();
+
+            MoveTo {
+                cmd_type,
+                start,
+                commands: commands,
+            }
         })
     )
 );
@@ -181,14 +214,14 @@ mod tests {
 
     #[test]
     fn test_moveto() {
-        assert_eq!(moveto(b"m 12.5,3.2").to_result().unwrap(), Point(12.5, 3.2),);
+        assert_eq!(moveto(b"M 12.5,3.2").to_result().unwrap(), (CommandType::Absolute, Point(12.5, 3.2)));
     }
 
     #[test]
     fn test_curveto_argument() {
         assert_eq!(
             curveto(b"c 1,1 2,2 3,3").to_result().unwrap(),
-            vec![DrawTo::CurveTo(Point(1., 1.), Point(2., 2.), Point(3., 3.))]
+            vec![DrawTo::CurveTo(CommandType::Relative, Point(1., 1.), Point(2., 2.), Point(3., 3.))]
         );
     }
 
@@ -198,11 +231,12 @@ mod tests {
         assert_eq!(
             moveto_drawto_command_group(path).to_result().unwrap(),
             MoveTo {
+                cmd_type: CommandType::Relative,
                 start: Point(12.5, 3.2),
-                drawto_commands: vec![
-                    DrawTo::LineTo(Point(5., 4.)),
-                    DrawTo::LineTo(Point(8., 2.)),
-                    DrawTo::CurveTo(Point(1., 1.), Point(2., 2.), Point(3., 3.)),
+                commands: vec![
+                    DrawTo::LineTo(CommandType::Relative, Point(5., 4.)),
+                    DrawTo::LineTo(CommandType::Relative, Point(8., 2.)),
+                    DrawTo::CurveTo(CommandType::Relative, Point(1., 1.), Point(2., 2.), Point(3., 3.)),
                     DrawTo::ClosePath,
                 ],
             }
@@ -211,17 +245,19 @@ mod tests {
 
     #[test]
     fn test_path() {
-        let path = "m 12.5 3.2 4 3 m 1 2 3 4";
+        let path = "M 12.5 3.2 4 3 m 1 2 3 4";
         assert_eq!(
             Path::parse(path),
             Ok(Path(vec![
                 MoveTo {
+                    cmd_type: CommandType::Absolute,
                     start: Point(12.5, 3.2),
-                    drawto_commands: vec![DrawTo::LineTo(Point(4., 3.))],
+                    commands: vec![DrawTo::LineTo(CommandType::Absolute, Point(4., 3.))],
                 },
                 MoveTo {
+                    cmd_type: CommandType::Relative,
                     start: Point(1., 2.),
-                    drawto_commands: vec![DrawTo::LineTo(Point(3., 4.))],
+                    commands: vec![DrawTo::LineTo(CommandType::Relative, Point(3., 4.))],
                 },
             ]))
         )
